5ae5a6dc3eb4a5c728cdc882db696d0b7761db08
Ramesh <pramesh@galaxe.com>
2024-04-26 22:04:06 +0530

    Max price code

---
+++ cypress/e2e/MaxPriceFromWebTable.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+
+describe('Finding max price', () => {
+    it('passes', () => {
+        let priceArray = [];
+        let maxPrice, TotalPrice = 0;
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('table#product:nth-child(1) tbody tr td:nth-child(4)').each(($price) => {
+            const text = parseInt($price.text());
+            priceArray.push(text);
+            maxPrice = Math.max(...priceArray);
+
+        }).then(() => {
+            for (let i = 0; i < priceArray.length; i++) {
+                TotalPrice += priceArray[i];
+            }
+            cy.log(`sum of price: ${TotalPrice}, maximum price course is:${maxPrice} and price array:${priceArray}`);
+            expect(maxPrice).to.equals(48);
+        })
+        cy.get('div.totalAmount').then(($el) => {
+            const collectedPrice = parseInt($el.text().split(':')[1].trim());
+            cy.log(collectedPrice);
+            expect(collectedPrice).to.equals(TotalPrice)
+        })
+    })
+
+})
