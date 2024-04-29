2c6912dfab0f2612397b28d9700e0a26b1dffa21
Raghvendra Pathak <136634191+TopperRaghu@users.noreply.github.com>
2024-04-29 16:32:06 +0530

    Update Second.js
    Updated Second.js

--- Second.js
+++ Second.js
@@ -2,6 +2,8 @@ const array1 = [1, 4, 9, 16];
 
 // Pass a function to map
 const map1 = array1.map((x) => x * 2);
-
+const map2 = array1.map((x) => x * 3);
 console.log(map1);
+console.log(map2);
 // Expected output: Array [2, 8, 18, 32]
+// Expected output: Array [3, 12, 27, 48]
